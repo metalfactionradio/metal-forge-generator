7512f9143140574de8aafacebe5406f44acd655b
metalfactionradio <metalfactionradio@gmail.com>
2026-06-09 10:54:05 -0400

    Fix head CDN links, Google sign-in only modal

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
 echo Copying index.html to www...
 copy index.html www\index.html
+
+echo Fixing CDN references in www\index.html...
+powershell -Command "(Get-Content www\index.html) -replace '<!-- saved from url.*?-->', '' -replace '<script src=""./index_files/saved_resource""></script>', '' -replace '<link rel=""stylesheet"" href=""./index_files/all.min.css"">', '<link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"">' -replace '<link href=""./index_files/css2"" rel=""stylesheet"">', '<link href=""https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Inter:wght@400;500;600;700&display=swap"" rel=""stylesheet"">' | Set-Content www\index.html"
+
 echo Syncing Capacitor...
 npx cap sync android
 echo Done!
